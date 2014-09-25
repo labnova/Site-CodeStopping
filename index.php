54de36a9030677b90962bedb7d870a220be48979
labnova <progettofeb@gmail.com>
2014-09-25 11:10:30 +0200

    primo commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<meta charset="utf-8">
+<head>
+  <link rel="stylesheet" href="style.css" />
+
+</head>
+<body>
+<canvas id="canvas"></canvas>
+<link href='http://fonts.googleapis.com/css?family=Varela' rel='stylesheet' type='text/css'>
+<body>
+<div class="glitch" data-text="Stopping">Code</div>  
+
+<script src="js/script.js"></script>
+<script src="http://d3js.org/d3.v3.min.js"></script>
+
+</body>
+</html>
+
